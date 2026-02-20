4d554de6e9901a73dbc6546b9fb9f48dfdf6523a
iacopo00 <iacopo.massei09@gmail.com>
2026-02-20 23:24:35 +0100

    Finished Chapter 4 on test bench and fixed the test bench file

--- tb/tb_writing_master.vhd
+++ tb/tb_writing_master.vhd
@@ -13,7 +13,7 @@ end entity;
 
 architecture behavior of tb_writing_master is
 
-    constant CLK_PERIOD : time := 20 ns;
+    constant CLK_PERIOD : time := 8 ns;
 
     -- component declaration
     component writing_master is
@@ -54,19 +54,18 @@ architecture behavior of tb_writing_master is
 
         p_STIMULUS: process 
         begin
+            -- ONE MESSAGE SCENARIO --
             -- 1. Reset device
             reset_ext <= '0';
             wait for CLK_PERIOD;
             reset_ext <= '1';
 
-            -- 2. Send data, addr and setting valid to 1
-            wait until rising_edge(clk_ext);
+            -- 2. Set data, addr and setting valid to 1
             addr_ext <= "1010101";
             data_ext <= "11001100";
             valid_ext <= '1';
-
+            wait until falling_edge(clk_ext);
             -- 3. Release valid
-            wait for CLK_PERIOD;
             valid_ext <= '0';
 
             -- 4. Wait for START condition (SDA goes low while SCL is high)
@@ -89,7 +88,7 @@ architecture behavior of tb_writing_master is
     
             -- 8. Send the ACK waiting enough time to be sure master received it
             sda_ext <= '0';
-            wait for 32 * CLK_PERIOD;
+            wait until falling_edge(scl_ext);
             sda_ext <= 'Z';
 
             -- 9. Receive data
@@ -102,59 +101,275 @@ architecture behavior of tb_writing_master is
             -- 10. Wait for the master to release the bus for the ACK phase
             wait until sda_ext = 'Z';
 
-            -- 11 & 12. Send the ACK AND simultaneously command the next byte
-            sda_ext <= '0'; -- Slave pulls SDA low for ACK
-            
-            -- Immediately assert the new command while holding the ACK
-            addr_ext <= "1010101";
-            data_ext <= "11001100";
-            valid_ext <= '1';
-        
-            -- 13. Release valid and sda
-            wait for 32 * CLK_PERIOD; 
-            valid_ext <= '0';
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
             sda_ext <= 'Z';
 
-            -- 14. Wait for REPEATED START condition (SDA goes low while SCL is high)
-            wait until falling_edge(sda_ext) and scl_ext = '1';
+            -- 11. Wait for STOP condition
+            wait until rising_edge(sda_ext) and scl_ext = '1';
+            report "One message scenario completed!";
 
-            -- 15. Verify the 7 Address bits (MSB first: index 6 down to 0)
+            -- MULTIPLE MESSAGES SCENARIO --
+            -- Same until point 10
+            wait for 8 * CLK_PERIOD;
+            addr_ext <= "1101010";
+            data_ext <= "00110011";
+            valid_ext <= '1';
+
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+            
             for i in 6 downto 0 loop
                 wait until rising_edge(scl_ext);
                 assert (sda_ext = addr_ext(i)) 
                     report "Error: No slaves match!" severity error;
             end loop;
 
-            -- 16. Verify the R/W bit (Write = '0')
+
             wait until rising_edge(scl_ext);
             assert (sda_ext = '0') 
                 report "Error: R/W bit should be '0' for writing!" severity error;
 
-            -- 17. Wait for the master to release the bus for the ACK phase
             wait until sda_ext = 'Z';
     
-            -- 18. Send the ACK
             sda_ext <= '0';
-            wait for 32 * CLK_PERIOD;
+            wait until falling_edge(scl_ext);
             sda_ext <= 'Z';
 
-            -- 19. Receive data
             for i in 7 downto 0 loop
                 wait until rising_edge(scl_ext);
                 assert (sda_ext = data_ext(i)) 
                     report "Error: Wrong data!" severity error;
             end loop;
 
-            -- 20. Wait for the master to release the bus for the ACK phase
             wait until sda_ext = 'Z';
 
-            -- 21. Send the ACK
+            -- Prepare data to transmit
+            addr_ext <= "1101010";
+            data_ext <= "10001011";
+            valid_ext <= '1';
+
             sda_ext <= '0';
-            wait for 32 * CLK_PERIOD;
+            wait until rising_edge(scl_ext);
             sda_ext <= 'Z';
 
-            -- 22. Wait for STOP condition (SDA goes high while SCL is high)
+            -- Second START
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+
+            for i in 6 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = addr_ext(i)) 
+                    report "Error: No slaves match!" severity error;
+            end loop;
+
+
+            wait until rising_edge(scl_ext);
+            assert (sda_ext = '0') 
+                report "Error: R/W bit should be '0' for writing!" severity error;
+
+
+            wait until sda_ext = 'Z';
+    
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            for i in 7 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = data_ext(i)) 
+                    report "Error: Wrong data!" severity error;
+            end loop;
+
+
+            wait until sda_ext = 'Z';
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
             wait until rising_edge(sda_ext) and scl_ext = '1';
+
+            -- MULTIPLE MESSAGES SCENARIO --
+            -- Same until point 10
+            wait for 8 * CLK_PERIOD;
+            addr_ext <= "1101010";
+            data_ext <= "00110011";
+            valid_ext <= '1';
+
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+            
+            for i in 6 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = addr_ext(i)) 
+                    report "Error: No slaves match!" severity error;
+            end loop;
+
+
+            wait until rising_edge(scl_ext);
+            assert (sda_ext = '0') 
+                report "Error: R/W bit should be '0' for writing!" severity error;
+
+            wait until sda_ext = 'Z';
+    
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            for i in 7 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = data_ext(i)) 
+                    report "Error: Wrong data!" severity error;
+            end loop;
+
+            wait until sda_ext = 'Z';
+
+            -- Prepare data to transmit
+            addr_ext <= "0010011";
+            data_ext <= "10001011";
+            valid_ext <= '1';
+
+            sda_ext <= '0';
+            wait until rising_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            -- Second START
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+
+            for i in 6 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = addr_ext(i)) 
+                    report "Error: No slaves match!" severity error;
+            end loop;
+
+
+            wait until rising_edge(scl_ext);
+            assert (sda_ext = '0') 
+                report "Error: R/W bit should be '0' for writing!" severity error;
+
+
+            wait until sda_ext = 'Z';
+    
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            for i in 7 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = data_ext(i)) 
+                    report "Error: Wrong data!" severity error;
+            end loop;
+
+
+            wait until sda_ext = 'Z';
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            wait until rising_edge(sda_ext) and scl_ext = '1';
+
+            report "Multiple messages scenario completed!";
+
+            -- NACK ADDR SCENARIO --
+            wait for 8 * CLK_PERIOD;
+            addr_ext <= "0110101";
+            data_ext <= "11011100";
+            valid_ext <= '1';
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+
+            for i in 6 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = addr_ext(i)) 
+                    report "Error: No slaves match!" severity error;
+            end loop;
+
+            wait until rising_edge(scl_ext);
+            assert (sda_ext = '0') 
+                report "Error: R/W bit should be '0' for writing!" severity error;
+
+            wait until sda_ext = 'Z';
+    
+            sda_ext <= '1'; -- NACK ADDR
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            wait until rising_edge(sda_ext) and scl_ext = '1';
+            report "NACK ADDR scenario completed!";
+
+            -- NACK DATA SCENARIO -- 
+            wait for 8 * CLK_PERIOD;
+            addr_ext <= "0110101";
+            data_ext <= "11011100";
+            valid_ext <= '1';
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+
+            for i in 6 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = addr_ext(i)) 
+                    report "Error: No slaves match!" severity error;
+            end loop;
+
+            wait until rising_edge(scl_ext);
+            assert (sda_ext = '0') 
+                report "Error: R/W bit should be '0' for writing!" severity error;
+
+            wait until sda_ext = 'Z';
+    
+            sda_ext <= '0';
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            for i in 7 downto 0 loop
+                wait until rising_edge(scl_ext);
+                assert (sda_ext = data_ext(i)) 
+                    report "Error: Wrong data!" severity error;
+            end loop;
+
+
+            wait until sda_ext = 'Z';
+            sda_ext <= '1'; -- NACK DATA
+            wait until falling_edge(scl_ext);
+            sda_ext <= 'Z';
+
+            wait until rising_edge(sda_ext) and scl_ext = '1';
+            report "NACK DATA scenario completed!";
+            
+            valid_ext <= '0';
+            addr_ext  <= (others => '0');
+            data_ext  <= (others => '0');
+
+            -- ASYNCHRONOUS RESET SCENARIO --
+            wait for 8 * CLK_PERIOD;
+            addr_ext <= "1010100";
+            data_ext <= "11110010";
+            valid_ext <= '1';
+            wait until falling_edge(sda_ext) and scl_ext = '1';
+            valid_ext <= '0';
+
+            for i in 1 to 10 loop
+                wait until rising_edge(scl_ext);
+            end loop;
+
+            wait for 3 ns;
+            reset_ext <= '0'; 
+
+            wait for 1 ns;
+            assert (sda_ext = '1' and scl_ext = '1') 
+                report "Error: master is not in IDLE state!" severity failure;
+
+            wait for 2 * CLK_PERIOD;
+            reset_ext <= '1';
+
+            report "ASYNCHRONOUS SCENARIO COMPLETED";
+
+            valid_ext <= '0';
+            addr_ext  <= (others => '0');
+            data_ext  <= (others => '0');
+
+            wait until rising_edge(clk_ext);
             wait for 8 * CLK_PERIOD;
             testing <= false;
             wait;
